97c916f0506755b274f74a33dcf84dd0c2bff1d6
Goulven-Molaret <goulven.molaret@gmail.com>
2018-06-05 19:11:51 +0800

    indexing

--- back/Json0.py
+++ back/Json0.py
@@ -9,10 +9,27 @@ song_metadata_file = 'song_data.csv'
 songs = pd.read_csv(song_metadata_file)
 
 songs['_id'] = songs['song_id']
-songs_short = songs.head(20)
+
 
 #print(songs_short)
 
+for i in range(0, songs.size, 20):
+	songs_short = songs[i:i+20]
+	df_as_json = songs_short.to_json(orient='records', lines=True)
+
+	final_json_string = ''
+	for json_document in df_as_json.split('\n'):
+	    jdict = json.loads(json_document)
+	    metadata = json.dumps({'index': {'_id': jdict['_id']}})
+	    jdict.pop('_id')
+	    final_json_string += metadata + '\n' + json.dumps(jdict) + '\n'
+
+	headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+	r = requests.post('http://localhost:9200/SongIndex/text/_bulk', data=final_json_string, headers=headers, timeout=60)
+
+	print(r.json())
+
+"""
 df_as_json = songs_short.to_json(orient='records', lines=True)
 
 
@@ -27,4 +44,4 @@ for json_document in df_as_json.split('\n'):
 headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 r = requests.post('http://localhost:9200/index0/my_type/_bulk', data=final_json_string, headers=headers, timeout=60)
 
-print(r.json())
+print(r.json())"""
