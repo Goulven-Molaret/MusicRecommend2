3eaf9d1d37fe2427a5919e4d6f6799314fa8682a
Goulven-Molaret <goulven.molaret@gmail.com>
2018-06-05 17:37:21 +0800

    background

--- run.py
+++ run.py
@@ -2,8 +2,8 @@
 from fantasticsearch import fantasticsearch
 
 
-
-fantasticsearch.run(debug=True, host='127.0.0.1', port=5000)
+if __name__ == '__main__':
+	fantasticsearch.run(debug=True, host='127.0.0.1', port=5000)
 
 
 
